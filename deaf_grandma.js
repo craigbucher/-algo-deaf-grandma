cbc4b7eb8165be4e7e959a133b15b7ab7d3e8924
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-23 17:32:30 -0500

    initial code push

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,5 +1,30 @@
-function deafGrandma() {
-
+function deafGrandma(){	
+  let byeCount = 0;	
+  let response;
+  const readline = require('readline')
+  const inquirer = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+  while (byeCount < 2){	
+    inquirer.question("What do you want to say to Grandma?", response);	
+    if (response === ''){	
+      console.log('WHAT?!');	
+    } else if (response === 'GOODBYE!' && byeCount < 1){	
+      console.log('LEAVING SO SOON?');	
+      byeCount ++;	
+    } else if (isUpperCase(response) && response !== 'GOODBYE!'){	
+      console.log('NO, NOT SINCE 1946!');	
+    } else if (!isUpperCase(response)){	
+      console.log('SPEAK UP, KID!');	
+    } else if (response === 'GOODBYE!'){	
+      console.log('LATER, SKATER!');	
+      return '';	
+    }	
+  }	
+}	
+function isUpperCase(string){	
+  return (string === string.toUpperCase());	
 }
 
 deafGrandma();
