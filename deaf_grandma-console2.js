54390a2d6ee9d3a50e49465a43fb1eb46760b476
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-24 11:12:06 -0500

    initial code

--- deaf_grandma-console2.js
+++ deaf_grandma-console2.js
@@ -1,5 +1,26 @@
 function deafGrandma(){	
- 
+  const readline = require('readline');
+  const rl = readline.createInterface(
+    process.stdin, process.stdout);
+  let bye = false;
+  let outputMessage = 'SPEAK UP, KID!';
+
+  readline.question('Grandma is listening...', input => {
+    if (input === ''){
+      outputMessage = 'WHAT?!';
+    } else if (input.match(/^[A-Z]/)){
+      outputMessage = 'NO, NOT SINCE 1946';
+    } else if (input.match(/^[GOODBYE]/)){
+      if (bye){
+        return 'LATER, SKATER!';
+        close();
+      }
+      outputMessage = 'LEAVING SO SOON?';
+      bye = true;
+    }
+    console.log(outputMessage);
+    deafGrandma();
+  });
 }
 
 
