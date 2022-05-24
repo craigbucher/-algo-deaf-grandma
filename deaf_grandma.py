642c7f6c86a351c522769444bdce18df0a35022f
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-23 21:58:28 -0500

    initial python code

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,5 +1,20 @@
 def deaf_grandma():
-
+  byeCount = 0
+  response = ''
+  while byeCount < 2:
+      response = input('What do you want to say to Grandma??')
+      if response == '':
+        print('WHAT?')
+      elif response == 'GOODBYE!' and byeCount < 1:
+        print('Leaving so soon?')
+        byeCount +=1
+      elif response.isupper() and response != 'GOODBYE!':
+        print('NO NOT SINCE 1946!')
+      elif not response.isupper():
+        print('SPEAK UP KID!')
+      else:
+        print('LATER SKATER!')
+        byeCount +=1
 
 
 deaf_grandma()
