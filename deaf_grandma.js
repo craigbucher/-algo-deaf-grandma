f90f696368f9ddc3146868529c562572ea3b8ff9
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-24 08:53:28 -0500

    first try using index.html

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,12 +1,26 @@
 function deafGrandma(){	
-  const rl = require('readline').createInterface({
-      input: process.stdin,
-      outpurt: process.stdout
-  });
-  rl.question('What is your favorite food? ', (answer) => {
-    console.log(`Oh, so your favorite food is ${answer}`);
-  });
+  let byeCount = 0;
+  let response;
+  while (byeCount < 2){
+    response = window.prompt('What would you like to say to Grandma?');
+    if (response === ''){	
+      window.prompt('WHAT?!');
+    } else if (response === 'GOODBYE!' && byeCount < 1){	
+      window.prompt('LEAVING SO SOON?');	
+      byeCount ++;
+    } else if (isUpperCase(response) && response !== 'GOODBYE!'){	
+      window.prompt('NO, NOT SINCE 1946!');
+    } else if (!isUpperCase(response)){	
+      window.prompt('SPEAK UP, KID!');
+    } else if (response === 'GOODBYE!'){	
+      window.prompt('LATER, SKATER!');	
+      return '';
+    }
+  }
+}
 
+function isUpperCase(string){	
+  return (string === string.toUpperCase());	
 }
 
 deafGrandma();
