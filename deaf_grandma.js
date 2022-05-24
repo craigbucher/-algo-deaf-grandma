8861c972f42857d93886a02416c2e5ff35e3d42b
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-23 20:40:25 -0500

    deleted

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,25 +1,12 @@
 function deafGrandma(){	
-  let byeCount = 0;	
-  let response;	
-  while (byeCount < 2){	
-    let response = prompt('What do you want to say to Grandma!!');	
-    if (response === ''){	
-      console.log('WHAT?!');	
-    } else if (response === 'GOODBYE!' && byeCount < 1){	
-      console.log('LEAVING SO SOON?');	
-      byeCount ++;	
-    } else if (isUpperCase(response) && response !== 'GOODBYE!'){	
-      console.log('NO, NOT SINCE 1946!');	
-    } else if (!isUpperCase(response)){	
-      console.log('SPEAK UP, KID!');	
-    } else if (response === 'GOODBYE!'){	
-      console.log('LATER, SKATER!');	
-      return '';	
-    }	
-  }	
-}	
-function isUpperCase(string){	
-  return (string === string.toUpperCase());	
+  const rl = require('readline').createInterface({
+      input: process.stdin,
+      outpurt: process.stdout
+  });
+  rl.question('What is your favorite food? ', (answer) => {
+    console.log(`Oh, so your favorite food is ${answer}`);
+  });
+
 }
 
 deafGrandma();
