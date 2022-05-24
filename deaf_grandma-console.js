9bdbfa0cf073c52ceec253816924154c9d52acc9
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-24 10:36:56 -0500

    This requires prompt module

--- deaf_grandma-console.js
+++ deaf_grandma-console.js
@@ -1,12 +1,31 @@
-function deafGrandma(){	
-  let byeCount = 0;
-  let response;
-  let readline = require('readline');
-  let rl = readline.createInterface(
-    process.stdin, process.stdout);
-  rl.question('What is your age? ', (age) => {
-  console.log('Your age is: ' + age);
-    });
+function deafGrandma(counter = 0){	
+  var prompt = require('prompt');
+  var output = 'Grandma is listening. . .'
+  prompt.start();
+  prompt.get('askGrandma', function(err, result){
+    var input = result.askGrandma;
+    if (counter < 3){
+      var input = result.askGrandma;
+      if (input === ''){
+        output = 'WHAT?';
+      } else if (/[a-z]/.test(input)){
+        output = 'SPEAK UP, KID!';
+      } else if (input === input.toUpperCase){
+        output = 'NO, NOT SINCE 1946!';
+      } else if (input == 'GOODBYE!'){
+        if (counter == 1){
+          output = 'LEAVING SO SOON?';
+          counter++
+        } else if (counter == 2){
+          output = 'LATER, SKATER!';
+          counter++
+        }
+      }
+      if (counter < 3){
+        deafGrandma(counter)
+      }
+    }
+  });
     
 }
 
