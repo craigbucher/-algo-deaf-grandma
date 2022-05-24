d3552b504bb52a85b6d76b5253a5743de469395e
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-24 09:30:42 -0500

    template for readline

---
+++ deaf_grandma-console.js
@@ -0,0 +1,13 @@
+function deafGrandma(){	
+  let byeCount = 0;
+  let response;
+  while (byeCount < 2){
+
+  }
+}
+
+function isUpperCase(string){	
+  return (string === string.toUpperCase());	
+}
+
+deafGrandma();
