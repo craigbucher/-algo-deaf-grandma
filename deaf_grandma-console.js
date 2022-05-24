4ed28c78a73926b08c771ed41067a2c8e5becf17
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-24 10:01:03 -0500

    This is a test

--- deaf_grandma-console.js
+++ deaf_grandma-console.js
@@ -1,9 +1,13 @@
 function deafGrandma(){	
   let byeCount = 0;
   let response;
-  while (byeCount < 2){
-
-  }
+  let readline = require('readline');
+  let rl = readline.createInterface(
+    process.stdin, process.stdout);
+  rl.question('What is your age? ', (age) => {
+  console.log('Your age is: ' + age);
+    });
+    
 }
 
 function isUpperCase(string){	
